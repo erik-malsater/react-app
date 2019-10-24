2a931ffb57d59421fdd7622a12b31f2c762f05d4
Erik Målsäter <erik.malsater92@gmail.com>
2019-10-24 18:02:09 +0200

    adds routing and basic html for HairdresserList

---
+++ src/components/HairdresserListComponent/HairdresserListComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class HairdresserListComponent extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <header>
+          
+        </header>
+        <main>
+
+        </main>
+        
+      </div>
+    )
+  }
+}
+export default HairdresserListComponent;
